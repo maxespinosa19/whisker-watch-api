f246a3ff3a405405a758a0e164cbe29523ad6cec
Max Espinosa <maxespinosaschool@gmail.com>
2023-07-25 12:25:38 -0400

    add created

--- functions/src/animals.js
+++ functions/src/animals.js
@@ -1,3 +1,4 @@
+
 import db from "./dbConnect.js"
 
 const coll = db.collection("animals")
@@ -9,4 +10,14 @@ export async function getAnimalForms(req,res) {
     doc.id, ...doc.data() }))
 
     res.send(animalFormsArray);
+}
+
+//add
+export async function addAnimal(req,res){
+  const {tag, name, imageURL,location, temperament, description } = req.body
+
+  const newAnimalForm = { tag, name, imageURL,location, temperament, description }
+
+  await coll.add(newAnimalForm); 
+  getAnimalForms(req, res);
 }
